ef90b58314208e7fe5b57c34374d333cda3180eb
David Kalnischkies <kalnischkies@gmail.com>
2011-08-11 20:53:28 +0200

    cppcheck complains about some possible speed improvements which could be done on the mirco-optimazation level, so lets fix them:  (performance) Possible inefficient checking for emptiness.  (performance) Prefer prefix ++/-- operators for non-primitive types.
    Debian: f7f0d6c7560a8f71707e7852a512469147aa9f84

--- methods/mirror.cc
+++ methods/mirror.cc
@@ -109,7 +109,7 @@ bool MirrorMethod::Clean(string Dir)
 	 continue;
 
       // see if we have that uri
-      for(I=list.begin(); I != list.end(); I++)
+      for(I=list.begin(); I != list.end(); ++I)
       {
 	 string uri = (*I)->GetURI();
 	 if(uri.find("mirror://") != 0)
@@ -332,7 +332,7 @@ string MirrorMethod::GetMirrorFileName(string mirror_uri_str)
    vector<metaIndex *>::const_iterator I;
    pkgSourceList list;
    list.ReadMainList();
-   for(I=list.begin(); I != list.end(); I++)
+   for(I=list.begin(); I != list.end(); ++I)
    {
       string uristr = (*I)->GetURI();
       if(Debug)
