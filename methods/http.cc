3c5a1f2739bc4d9f758b7ac852715e015dbd1f18
David Kalnischkies <kalnischkies@gmail.com>
2011-09-13 10:09:00 +0200

    reorder includes: add <config.h> if needed and include it at first
    Debian: ea54214002c09eeb4dd498d97a564471ec9993c5

--- methods/http.cc
+++ methods/http.cc
@@ -25,6 +25,8 @@
    ##################################################################### */
 									/*}}}*/
 // Include Files							/*{{{*/
+#include <config.h>
+
 #include <apt-pkg/fileutl.h>
 #include <apt-pkg/acquire-method.h>
 #include <apt-pkg/error.h>
@@ -40,8 +42,6 @@
 #include <errno.h>
 #include <string.h>
 #include <map>
-#include <apti18n.h>
-
 
 // Internet stuff
 #include <netdb.h>
@@ -50,6 +50,8 @@
 #include "connect.h"
 #include "rfc2553emu.h"
 #include "http.h"
+
+#include <apti18n.h>
 									/*}}}*/
 using namespace std;
 
