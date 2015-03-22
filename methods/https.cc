f85f9f179c81f2621e0031e3096f80878f03f61c
Robert Edmonds <edmonds@debian.org>
2015-03-22 00:12:45 -0400

    HttpsMethod::Fetch(): Zero the FetchResult object when leaving due to 404
    Debian: 1296bc7c466181a7978c313c40a041b34ce3eaeb

--- methods/https.cc
+++ methods/https.cc
@@ -444,6 +444,8 @@ bool HttpsMethod::Fetch(FetchItem *Itm)
       _error->Error("%i %s", Server->Result, Server->Code);
       // unlink, no need keep 401/404 page content in partial/
       unlink(File->Name().c_str());
+      Res.Size = 0;
+      Res.LastModified = 0;
       return false;
    }
 
