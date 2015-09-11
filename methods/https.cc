5fa9cde5df2c08760ec80a25a92ef2d0e6cc5256
David Kalnischkies <david@kalnischkies.de>
2015-09-11 21:02:19 +0200

    fix two memory leaks reported by gcc
    Reported-By: gcc -fsanitize=address -fno-sanitize=vptr
    Git-Dch: Ignore
    
    
    Debian: 830a1b8c9e9a26dc1101167ac66a75c444902c4d

--- methods/https.cc
+++ methods/https.cc
@@ -502,9 +502,9 @@ bool HttpsMethod::Configuration(string Message)
    return true;
 }
 									/*}}}*/
-ServerState * HttpsMethod::CreateServerState(URI uri)			/*{{{*/
+std::unique_ptr<ServerState> HttpsMethod::CreateServerState(URI const &uri)/*{{{*/
 {
-   return new HttpsServerState(uri, this);
+   return std::unique_ptr<ServerState>(new HttpsServerState(uri, this));
 }
 									/*}}}*/
 
