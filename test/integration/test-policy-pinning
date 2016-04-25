Debian/apt

commit 69cea1ef2cfda3c4da79fd756a8edaf2be26998e

give rc-status packages a pin of -1
It would previously return a pin of 0, which is an invalid value, but
the intend is that versions which are only in the dpkg/status file can't
be selected for installation (= can't be a candidate) which is what a
negative pin assures.

This helps with the communication to EDSP solvers as they neither know
about the rc-state (yet) nor that they shouldn't choose this version.
Ideally they shouldn't be told about such versions at all as there is
nothing to be solved here, but we will get there eventually.

apt-pkg/policy.cc
@@ -374,7 +374,7 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver, b
    if (!ConsiderFiles)
       return 0;
 
-   int priority = std::numeric_limits<int>::min();
+   signed short priority = std::numeric_limits<signed short>::min();
    for (pkgCache::VerFileIterator file = Ver.FileList(); file.end() == false; file++)
    {
       /* If this is the status file, and the current version is not the
@@ -382,14 +382,13 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver, b
          then it is not a candidate for installation, ever. This weeds
          out bogus entries that may be due to config-file states, or
          other. */
-      if (file.File().Flagged(pkgCache::Flag::NotSource) && Ver.ParentPkg().CurrentVer() != Ver) {
-	 // Ignore
-      } else if (GetPriority(file.File()) > priority) {
-	 priority = GetPriority(file.File());
-      }
+      if (file.File().Flagged(pkgCache::Flag::NotSource) && Ver.ParentPkg().CurrentVer() != Ver)
+	 priority = std::max(priority, static_cast<decltype(priority)>(-1));
+      else
+	 priority = std::max(priority, GetPriority(file.File()));
    }
 
-   return priority == std::numeric_limits<int>::min() ? 0 : priority;
+   return priority == std::numeric_limits<decltype(priority)>::min() ? 0 : priority;
 }
 APT_PURE signed short pkgPolicy::GetPriority(pkgCache::PkgFileIterator const &File)
 {

imz/apt-rpm_next

commit 54990470a93e5ce9f78ce136ca1e961b29422883

restore pinning to min/max value of short
Broken in the previous commit (69cea1ef2cfda3c4da79fd756a8edaf2be26998e).
Adding a test and a comment to avoid future embarrassment.

Git-Dch: Ignore
Reported-By: Julian Andres Klode on IRC


Debian: fd2e438b24f4ce153ee35a691ae5bcb7eb28cc98

apt-pkg/policy.cc
@@ -374,7 +374,8 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver, b
    if (!ConsiderFiles)
       return 0;
 
-   signed short priority = std::numeric_limits<signed short>::min();
+   // priorities are short ints, but we want to pick a value outside the valid range here
+   auto priority = std::numeric_limits<signed int>::min();
    for (pkgCache::VerFileIterator file = Ver.FileList(); file.end() == false; file++)
    {
       /* If this is the status file, and the current version is not the
@@ -385,7 +386,7 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::VerIterator const &Ver, b
       if (file.File().Flagged(pkgCache::Flag::NotSource) && Ver.ParentPkg().CurrentVer() != Ver)
 	 priority = std::max(priority, static_cast<decltype(priority)>(-1));
       else
-	 priority = std::max(priority, GetPriority(file.File()));
+	 priority = std::max(priority, static_cast<decltype(priority)>(GetPriority(file.File())));
    }
 
    return priority == std::numeric_limits<decltype(priority)>::min() ? 0 : priority;

test/integration/test-policy-pinning
@@ -273,7 +273,6 @@ Pin-Priority: -32768
 " > rootdir/etc/apt/preferences
 testsuccess aptget install -s coolstuff -o PinPriority=-32768
 
-
 # Check for 32-bit integers
 echo "Package: coolstuff
 Pin: release n=backports
@@ -305,6 +304,26 @@ testfailureequal "Reading package lists...
 E: Cannot convert 2147483648 to integer: out of range
 E: ${tmppath}/rootdir/etc/apt/preferences: Value 2147483648 is outside the range of valid pin priorities (-32768 to 32767)" \
  aptget install -s coolstuff -o PinPriority=2147483648
+
+# Check that short-max/min is a valid pin
+currentpin() {
+echo "Package: *
+Pin: release n=backports
+Pin-Priority: $1
+" > rootdir/etc/apt/preferences
+testsuccessequal "coolstuff:
+  Installed: 2.0~bpo1
+  Candidate: $2
+  Version table:
+     2.0~bpo2 $1
+       $1 file:${tmppath}/aptarchive backports/main all Packages
+ *** 2.0~bpo1 100
+        100 ${tmppath}/rootdir/var/lib/dpkg/status
+     1.0 500
+        500 file:${tmppath}/aptarchive stable/main all Packages" apt policy coolstuff
+}
+currentpin '32767' '2.0~bpo2'
+currentpin '-32768' '2.0~bpo1'
 
 # Check for 0
 echo "Package: coolstuff