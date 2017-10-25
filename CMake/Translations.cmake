Debian/apt

commit 43b9eb5bac15666fdc0346aca7031fab0fa5e064

CMake: methods: Cleanup link libraries, use OBJECT libraries
Use OBJECT libraries for http and connect stuff, and move the
seccomp link expression into a global link_libraries() call.

This also fixes a bug where only the http target pulled in
the gnutls header arguments despite gnutls being used in
connect.cc, and thus by mirror and ftp as well.

Adjust translation support to ignore TARGET_OBJECTS sources
and add the OBJECT libraries to the translated files.

CMake/Translations.cmake
@@ -31,6 +31,11 @@ function(apt_add_translation_domain)
         get_target_property(source_dir ${target} SOURCE_DIR)
         get_target_property(sources ${target} SOURCES)
         foreach(source ${sources})
+            if (source MATCHES TARGET_OBJECTS)
+                message(IGNORE ${source})
+                continue()
+            endif()
+            message(ADD ${source})
             path_join(file "${source_dir}" "${source}")
             file(RELATIVE_PATH relfile ${PROJECT_SOURCE_DIR} ${file})
             set(files ${files} ${relfile})

methods/CMakeLists.txt
@@ -1,31 +1,28 @@
 # Create the executable targets
 include_directories($<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_INCLUDE_DIR}>)
+link_libraries(apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
+
+add_library(httplib OBJECT http.cc basehttp.cc)
+add_library(connectlib OBJECT connect.cc rfc2553emu.cc)
 
 add_executable(file file.cc)
 add_executable(copy copy.cc)
 add_executable(store store.cc)
 add_executable(gpgv gpgv.cc)
 add_executable(cdrom cdrom.cc)
-add_executable(http http.cc http_main.cc rfc2553emu.cc connect.cc basehttp.cc)
-add_executable(mirror mirror.cc http.cc rfc2553emu.cc connect.cc basehttp.cc)
-add_executable(ftp ftp.cc rfc2553emu.cc connect.cc)
+add_executable(http http_main.cc $<TARGET_OBJECTS:httplib> $<TARGET_OBJECTS:connectlib>)
+add_executable(mirror mirror.cc $<TARGET_OBJECTS:httplib> $<TARGET_OBJECTS:connectlib>)
+add_executable(ftp ftp.cc $<TARGET_OBJECTS:connectlib>)
 add_executable(rred rred.cc)
 add_executable(rsh rsh.cc)
 
-target_compile_definitions(http PRIVATE ${GNUTLS_DEFINITIONS})
-target_include_directories(http PRIVATE ${GNUTLS_INCLUDE_DIR})
+target_compile_definitions(connectlib PRIVATE ${GNUTLS_DEFINITIONS})
+target_include_directories(connectlib PRIVATE ${GNUTLS_INCLUDE_DIR})
 
-# Link the executables against the libraries
-target_link_libraries(file apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(copy apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(store apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(gpgv apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(cdrom apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(http apt-pkg ${GNUTLS_LIBRARIES} $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(mirror apt-pkg ${RESOLV_LIBRARIES} ${GNUTLS_LIBRARIES} $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(ftp apt-pkg ${GNUTLS_LIBRARIES} $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(rred apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
-target_link_libraries(rsh apt-pkg $<$<BOOL:${SECCOMP_FOUND}>:${SECCOMP_LIBRARIES}>)
+# Additional libraries to link against for networked stuff
+target_link_libraries(http ${GNUTLS_LIBRARIES})
+target_link_libraries(mirror ${RESOLV_LIBRARIES} ${GNUTLS_LIBRARIES})
+target_link_libraries(ftp ${GNUTLS_LIBRARIES})
 
 # Install the library
 install(TARGETS file copy store gpgv cdrom http ftp rred rsh mirror

po/CMakeLists.txt
@@ -15,7 +15,7 @@ apt_add_translation_domain(
     TARGETS apt apt-cache apt-get apt-config apt-cdrom apt-helper apt-mark
             apt-private
             # Methods
-            file copy store gpgv cdrom http ftp rred rsh mirror
+            connectlib httplib file copy store gpgv cdrom http ftp rred rsh mirror
     SCRIPTS ../dselect/install ../dselect/update
     EXCLUDE_LANGUAGES ${languages_excluded}
 )

imz/apt-rpm_next

commit 7038636f7be6c34666f3d99a6e4a9228ada4c282

CMake: Get rid of some debugging messages
This fixes a regression introduced in

commit 43b9eb5bac15666fdc0346aca7031fab0fa5e064

  CMake: methods: Cleanup link libraries, use OBJECT libraries

Gbp-Dch: ignore


Debian: 924eaf3c1a7f7d002a151b173805864b54671371

CMake/Translations.cmake
@@ -32,10 +32,8 @@ function(apt_add_translation_domain)
         get_target_property(sources ${target} SOURCES)
         foreach(source ${sources})
             if (source MATCHES TARGET_OBJECTS)
-                message(IGNORE ${source})
                 continue()
             endif()
-            message(ADD ${source})
             path_join(file "${source_dir}" "${source}")
             file(RELATIVE_PATH relfile ${PROJECT_SOURCE_DIR} ${file})
             set(files ${files} ${relfile})